50859f3323773dd44846c86eec7077fab7ee8352
melissapatez <m.patez@teltecsolutions.com.br>
2021-02-18 15:04:24 -0300

    Exemplo sobre condicao entre valores

---
+++ introducao JS/expressoesOperadores.js
@@ -0,0 +1,11 @@
+    //condição entre valor 1 se nao valor 2
+    // condition ? value1 : value2
+
+    let pao = true
+    let queijo = false
+    const niceBreakfast = pao && queijo ? 'cafe top' : 'cafe ruim'
+    console.log(niceBreakfast)
+
+    let age = 16
+    const canDrive = age >= 18 ? "can drive" : "can't drive"
+    console.log(canDrive)
