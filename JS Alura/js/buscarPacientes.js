4576cbeadd20ad5d76d886a578fc84a09c81e9f4
melissapatez <m.patez@teltecsolutions.com.br>
2021-03-29 12:20:27 -0300

    Capturando erros de requisição

--- JS Alura/js/buscarPacientes.js
+++ JS Alura/js/buscarPacientes.js
@@ -3,14 +3,24 @@ var botao = document.querySelector("#buscar-paciente");
 botao.addEventListener("click", function(){
     
     var xhr = new XMLHttpRequest(); // obj responsavel de fazer req http 
-    xhr.open("GET","https://api-pacientes.herokuapp.com/pacientes");
-    xhr.addEventListener("load", function(){
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta); // pega to do o texto da api e transforma em array
+    xhr.open("GET","https://api-pacientes.heroku1app.com/pacientes");
+    xhr.addEventListener("load", function() {
+    var errorAjax = document.querySelector("#error-busca");
+        if (xhr.status == 200) {
+            errorAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
 
-        pacientes.forEach(function(paciente){
-            addPacienteNaTabela(paciente);
-        });
+            pacientes.forEach(function(paciente) {
+                addPacienteNaTabela(paciente);
+            });
+        } else {
+            errorAjax.classList.remove("invisivel");
+        }
     });
+ 
     xhr.send();
 });
+           
+               
+    
