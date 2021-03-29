c712967b44d8378061ac3e30f3e70260c2186082
melissapatez <m.patez@teltecsolutions.com.br>
2021-03-29 12:26:27 -0300

    Ajustando link da api e realocando arquivos

--- JS Alura/Curso1 - tabela de pacientes/js/buscarPacientes.js
+++ JS Alura/Curso1 - tabela de pacientes/js/buscarPacientes.js
@@ -3,7 +3,7 @@ var botao = document.querySelector("#buscar-paciente");
 botao.addEventListener("click", function(){
     
     var xhr = new XMLHttpRequest(); // obj responsavel de fazer req http 
-    xhr.open("GET","https://api-pacientes.heroku1app.com/pacientes");
+    xhr.open("GET","https://api-pacientes.herokuapp.com/pacientes");
     xhr.addEventListener("load", function() {
     var errorAjax = document.querySelector("#error-busca");
         if (xhr.status == 200) {
